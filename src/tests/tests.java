5cca0e9e8d9e8c3b92cb84c90a190b4a612fb1d3
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-10-15 10:20:19 -0600

    lil change

--- src/tests/tests.java
+++ src/tests/tests.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- * tests n stuff
+ * sorting algos test
  *  - checks reference (ascending),
  *  - checks reversed reference (descending),
  *  - or is incorrect.
